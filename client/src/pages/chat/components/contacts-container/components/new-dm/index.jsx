1c77639cb0f62e586c6e7a5e4cb8304d96058d39
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-05-23 15:31:09 +0500

    Direct Messages Modal

---
+++ client/src/pages/chat/components/contacts-container/components/new-dm/index.jsx
@@ -0,0 +1,76 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { animationDefaultOptions } from "@/lib/utils";
+import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import Lottie from "react-lottie";
+
+function NewDM() {
+  const [openNewContactModal, setOpenNewContactModal] = useState(false);
+  const [searchedContacts, setSearchedContacts] = useState([]);
+
+  const searchContacts = async (searchTerm) => {};
+  return (
+    <>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <FaPlus
+              className="text-neutral-500 font-light text-opacity-90 text-start hover:text-neutral-100 cursor-pointer transition-all duration-300"
+              onClick={() => setOpenNewContactModal(true)}
+            />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Select New Contact</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <Dialog open={openNewContactModal} onOpenChange={setOpenNewContactModal}>
+        <DialogContent className="bg-[#181920] border-none text-white w-[400px] h-[400px] flex flex-col">
+          <DialogHeader>
+            <DialogTitle>Please select a contact</DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <div>
+            <Input
+              placeholder="Search for a contact"
+              className="rounded-lg bg-[#2c2e3b] border-none"
+              onChange={(e) => searchContacts(e.target.value)}
+            />
+          </div>
+          {searchedContacts.length <= 0 && (
+            <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center  duration-1000 transition-all mt-5">
+              <Lottie
+                isClickToPauseDisabled={true}
+                height={100}
+                width={100}
+                options={animationDefaultOptions}
+              />
+              <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-5 lg:text-2xl text-xl transition-all duration-300 text-center">
+                <h3 className="poppins-medium">
+                  Hi<span className="text-purple-500">!</span> Search new
+                  <span className="text-purple-500"> Contact.</span>
+                </h3>
+              </div>
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
+
+export default NewDM;
